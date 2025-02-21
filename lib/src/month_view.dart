0634cbcfa1502018a1514386a52e73be7e8dcc59
LAPTOP-S93F8J7K\asus <suemeanmhrzn100@gmail.com>
2025-02-21 12:29:49 +0545

    6

--- lib/src/month_view.dart
+++ lib/src/month_view.dart
@@ -211,70 +211,67 @@ class _MonthViewState extends State<_MonthView>
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: _kMaxDayPickerHeight,
-      child: Column(
-        children: <Widget>[
-          _CalendarHeader(
-            onHeaderLongPressed: widget.onHeaderLongPressed,
-            onHeaderTapped: widget.onHeaderTapped,
-            language: widget.language,
-            handleNextMonth: _handleNextMonth,
-            handlePreviousMonth: _handlePreviousMonth,
-            headerStyle: widget.headerStyle,
-            chevronOpacityAnimation: _chevronOpacityAnimation,
-            isDisplayingFirstMonth: _isDisplayingFirstMonth,
-            previousMonthDate: _previousMonthDate,
-            date: _currentDisplayedMonthDate,
-            isDisplayingLastMonth: _isDisplayingLastMonth,
-            nextMonthDate: _nextMonthDate,
-            changeToToday: () {
-              widget.onChanged(NepaliDateTime.now());
-            },
-            headerBuilder: widget.headerBuilder,
-          ),
-          Expanded(
-            child: Stack(
-              children: <Widget>[
-                Semantics(
-                  sortKey: _MonthPickerSortKey.calendar,
-                  child: NotificationListener<ScrollStartNotification>(
+    return Column(
+      children: <Widget>[
+        _CalendarHeader(
+          onHeaderLongPressed: widget.onHeaderLongPressed,
+          onHeaderTapped: widget.onHeaderTapped,
+          language: widget.language,
+          handleNextMonth: _handleNextMonth,
+          handlePreviousMonth: _handlePreviousMonth,
+          headerStyle: widget.headerStyle,
+          chevronOpacityAnimation: _chevronOpacityAnimation,
+          isDisplayingFirstMonth: _isDisplayingFirstMonth,
+          previousMonthDate: _previousMonthDate,
+          date: _currentDisplayedMonthDate,
+          isDisplayingLastMonth: _isDisplayingLastMonth,
+          nextMonthDate: _nextMonthDate,
+          changeToToday: () {
+            widget.onChanged(NepaliDateTime.now());
+          },
+          headerBuilder: widget.headerBuilder,
+        ),
+        Expanded(
+          child: Stack(
+            children: <Widget>[
+              Semantics(
+                sortKey: _MonthPickerSortKey.calendar,
+                child: NotificationListener<ScrollStartNotification>(
+                  onNotification: (_) {
+                    _chevronOpacityController.forward();
+                    return false;
+                  },
+                  child: NotificationListener<ScrollEndNotification>(
                     onNotification: (_) {
-                      _chevronOpacityController.forward();
+                      _chevronOpacityController.reverse();
                       return false;
                     },
-                    child: NotificationListener<ScrollEndNotification>(
-                      onNotification: (_) {
-                        _chevronOpacityController.reverse();
-                        return false;
-                      },
-                      child: PageView.builder(
-                        dragStartBehavior: widget.dragStartBehavior,
-                        key: ValueKey<NepaliDateTime>(widget.selectedDate),
-                        controller: _dayPickerController,
-                        scrollDirection: Axis.horizontal,
-                        itemCount:
-                            _monthDelta(widget.firstDate, widget.lastDate) + 1,
-                        itemBuilder: _buildItems,
-                        onPageChanged: _handleMonthPageChanged,
-                      ),
+                    child: PageView.builder(
+                      dragStartBehavior: widget.dragStartBehavior,
+                      key: ValueKey<NepaliDateTime>(widget.selectedDate),
+                      controller: _dayPickerController,
+                      scrollDirection: Axis.horizontal,
+                      itemCount:
+                          _monthDelta(widget.firstDate, widget.lastDate) + 1,
+                      itemBuilder: _buildItems,
+                      onPageChanged: _handleMonthPageChanged,
                     ),
                   ),
                 ),
-                /*  PositionedDirectional(
-                  top: 0.0,
-                  start: 8.0,
-                ), */
-                /* PositionedDirectional(
-                  top: 0.0,
-                  end: 8.0,
-                  child: 
-                ), */
-              ],
-            ),
+              ),
+              /*  PositionedDirectional(
+                top: 0.0,
+                start: 8.0,
+              ), */
+              /* PositionedDirectional(
+                top: 0.0,
+                end: 8.0,
+                child: 
+              ), */
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
